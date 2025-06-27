15a49a793766e14abb0313c2e178ec9f9729e3c9
deepansh1dw <Deepanshdwivedi8@gmail.com>
2025-06-27 20:08:38 +0530

    updated frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,30 +1,36 @@
-import React, { useEffect } from 'react';
-import { useProductStore } from '../store/useProductStore'
-import { PlusCircleIcon, RefreshCwIcon , PackageIcon } from 'lucide-react';
-import ProductCard from '../components/ProductCard';
-
-
+import { useEffect } from "react";
+import { useProductStore } from "../store/useProductStore";
+import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import ProductCard from "../components/ProductCard";
+import AddProductModal from "../components/AddProductModal";
 
 function HomePage() {
-  const { products , loading , error , fetchProducts} = useProductStore();
+  const { products, loading, error, fetchProducts } = useProductStore();
 
   useEffect(() => {
     fetchProducts();
-  } , [fetchProducts]);
+  }, [fetchProducts]);
+
   return (
-    <main className = "max-w-6xl mx-auto px-4 py-8">
-       <div className = "flex justify-between items-center mb-8">
-        <button className = "btn btn-primary">
-           <PlusCircleIcon className = "size-5 mr-2" />
-           add product
+    <main className="max-w-6xl mx-auto px-4 py-8 ">
+      <div className="flex justify-between items-center mb-8">
+        <button
+          className="btn btn-primary"
+          onClick={() => document.getElementById("add_product_modal").showModal()}
+        >
+          <PlusCircleIcon className="size-5 mr-2" />
+          Add Product
         </button>
         <button className="btn btn-ghost btn-circle" onClick={fetchProducts}>
-           <RefreshCwIcon className = "size-5" />
+          <RefreshCwIcon className="size-5" />
         </button>
-       </div>
+      </div>
 
-       {error && <div className="alert alert-error mb-8">{error}</div>}
-       {products.length === 0 && !loading && (
+      <AddProductModal />
+
+      {error && <div className="alert alert-error mb-8">{error}</div>}
+
+      {products.length === 0 && !loading && (
         <div className="flex flex-col justify-center items-center h-96 space-y-4">
           <div className="bg-base-100 rounded-full p-6">
             <PackageIcon className="size-12" />
@@ -38,21 +44,18 @@ function HomePage() {
         </div>
       )}
 
-       {loading ? (
-              <div className="flex justify-center items-center h-64">
-              <div className="loading loading-spinner loading-lg"></div>
-              </div>
-          ) : (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {products.map(product => (
-              <ProductCard key={product.id} product={product} />
-               ))}
-             </div>
-              )}
-
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <div className="loading loading-spinner loading-lg" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </main>
-    
-  )
+  );
 }
-
-export default HomePage
+export default HomePage;
